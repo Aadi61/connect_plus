89cb82ca1d035e5a05ec4cc17523ffa8c006122d
Aaryav Gangrade <aaryav1805.gangrade@gmail.com>
2024-04-09 18:59:19 +0530

    doctor_appointment2

--- firebase/doctor_appointment.py
+++ firebase/doctor_appointment.py
@@ -35,7 +35,8 @@ def retreiveTimeList(doctor_id, date):
     timingsInt = db.collection(doctor_collection).document(doctor_id).get().to_dict().get("slots")[date-1]
     timingsList = []
     for i in range(0, 24):
-        if timingsInt[i] > 0:
+        j = str(i)
+        if timingsInt[j] > 0:
             timingsList.append(i)
     return timingsList
 
@@ -52,27 +53,16 @@ def get_all_doctor_details():
         })
     return doctor_details
 
-#Add appointment details to the database
-# def add_appointment_detail(doctor_id, user_id, slots):
-#     appointment_ref = db.collection(appointment_collection).document()
-#     appointment_ref.set({
-#         "doctor_id": doctor_id,
-#         "user_id": user_id,
-#         "slots": slots
-#     })
-#     appointment_id = appointment_ref.id
-#     appointment_ref.update({"appointment_id": appointment_id})
-#     return appointment_id
-
 # Book function
 def book(date, time, doctor_id, user_id):
     date_time = db.collection(doctor_collection).document(doctor_id).get().to_dict().get("slots")
     timingsList = date_time[date-1]
-    if (timingsList[time] > 0):
-        timingsList[time] -= 1
+    timeStr = str(time)
+    if (timingsList[timeStr] > 0):
+        timingsList[timeStr] -= 1
         db.collection(doctor_collection).document(doctor_id).update({"slots":date_time})
-        appointmentTime = "{:02d}:{:02d}".format(time, 10*(5-timingsList[time]))
-        appointment_ref = db.collection(appointment_ref).document()
+        appointmentTime = "{:02d}:{:02d}".format(time, 10*(5-timingsList[timeStr]))
+        appointment_ref = db.collection(appointment_collection).document()
         appointment_ref.set({
             "date":date,
             "time":appointmentTime,
@@ -94,7 +84,3 @@ def getHistory(user_id):
     history = db.collection(user_collection).document(user_id).get().to_dict().get("appointment_history")
     return history
 
-
-d_id = add_doctor_detail("Dr. A", "Venue A", "General", [[0,0,0,6,6], [6,0,0,0,6],[6,6,6,6,6],[6,6,6,6,6],[6,6,6,0,6]])
-u_id = add_user_detail("User A", "aa@gmail.com", "1234", [])
-book(1, 3, d_id, u_id)
