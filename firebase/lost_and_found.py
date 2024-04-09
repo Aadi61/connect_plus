6082aca53c37728345b6a2746e2071c5303f4199
Aditya Mukati <adityamukati8564@gmail.com>
2024-04-09 14:48:05 +0530

    lost items done

--- firebase/lost_and_found.py
+++ firebase/lost_and_found.py
@@ -20,14 +20,12 @@ def add_found_item(name, image_url, place,contact,user_id):
     found_ref.update({
         "found_id":found_id
     })
-    print("Founded item added successfully with ID:", found_id)
     
 
 # Retreive a particular user's found items
 def retreive_found_item(user_id):
     found_ref = db.collection(found_collection)
     found_item = found_ref.get()
-    print(found_item)
     found_items_list = []
     for item in found_item:
         dict_item = item.to_dict()
@@ -42,7 +40,6 @@ def retreive_found_item(user_id):
 def retreive_all_found_items():
     found_ref = db.collection(found_collection)
     found_items = found_ref.get()
-    print("found items: ",found_items)
     found_items_list = []
     for found_item in found_items:
         found_items_list.append(found_item.to_dict())
@@ -54,4 +51,71 @@ def save_image_to_firebase(filename):
     blob.upload_from_filename(filename)
     blob.make_public()
     return blob.public_url
+
+
+# Delete a specific item using found_id
+def delete_item(found_id):
+    found_ref = db.collection(found_collection).document(found_id)
+    found_ref.delete()
     
+    
+    
+# Delete a specific item using lost_id
+def delete_item(lost_id):
+    found_ref = db.collection(lost_collection).document(lost_id)
+    found_ref.delete()
+    return True
+
+
+
+# Add a lost item to the database
+def add_lost_item(name, image_url, place, contact, user_id, date_lost):
+    lost_ref = db.collection(lost_collection).document()
+    lost_ref.set({
+        "name": name,
+        "image_url": image_url,
+        "place": place,
+        "contact": contact,
+        "user_id": user_id,
+        "date_lost": date_lost,  # Changed field name to date_lost
+    })
+    lost_id = lost_ref.id
+    lost_ref.update({"lost_id": lost_id})  # Changed field name to lost_id
+    return lost_id
+    
+    
+# Retrieve all lost items
+def retrieve_all_lost_items():
+    lost_ref = db.collection(lost_collection)
+    lost_items = lost_ref.get()
+    print("lost items: ", lost_items)
+    lost_items_list = []
+    for lost_item in lost_items:
+        lost_items_list.append(lost_item.to_dict())
+    return lost_items_list
+
+
+
+# Retreive a particular user's lost items
+def retrieve_lost_item(user_id):
+    lost_ref = db.collection(lost_collection)
+    lost_item = lost_ref.get()
+    print(lost_item)
+    lost_items_list = []
+    for item in lost_item:
+        dict_item = item.to_dict()
+        if dict_item['user_id'] == user_id:
+            print(dict_item)
+            lost_items_list.append(dict_item)
+    return lost_items_list
+
+
+
+# Mark an item as returned
+def mark_item_as_returned2(found_id):
+    found_ref = db.collection(found_collection).document(found_id)
+    found_ref.update({"returned": True})
+    return True
+
+
+
